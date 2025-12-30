328f09a20cf737989631357dfa99c93ef1193cce
Johan Rangel <johan.apr99@gmail.com>
2025-12-30 11:12:01 +0000

    new animal

--- app/main.py
+++ app/main.py
@@ -11,14 +11,14 @@ class Animal:
         Animal.animals.add(self)
     
     def print_name(self) -> str:
-        return f"Hello, I'am {self.name}"
+        return f"Hello, I'm {self.name}"
 
-    def seed(self) -> int:
+    def feed(self) -> int:
         
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
-            return self.appetite
+            return int(self.appetite)
         return 0
 
     def __repr__(self):
@@ -31,7 +31,7 @@ class Cat(Animal):
                  ) -> None:
         self.name = name
         self.is_hungry = is_hungry
-        super().__init__(self.name, 3)
+        super().__init__(self.name, 3, self.is_hungry)
     
     def catch_mouse(self):
         print("The hunt began!")
@@ -43,11 +43,11 @@ class Dog(Animal):
                  ) -> None:
         self.name = name
         self.is_hungry = is_hungry
-        super().__init__(self.name, 7)
+        super().__init__(self.name, 7, self.is_hungry)
     
     def bring_slippers(self):
         print("The slippers delivered!")
 
 def feed_animals(animals : list) -> int:
-    suma = sum(x.appetite for x in animals)
+    suma = sum(x.feed() for x in animals)
     print(suma)
